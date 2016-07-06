c68380aa7890367442a832f102534224ad446a58
SUM2 <alonshkalim@gmail.com>
2016-07-06 19:33:35 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -1,7 +1,14 @@
-"""
-Write a program that randomizes a number
-and prints the sum total of its digits.
-For example if the number was: 2345
-The result should be: 14
-"""
+
+from random import randint
+number = randint(1,10001)
+numberstr = str(number)
+
+sum = 0
+for i in numberstr:
+    sum += int(i)
+
+print numberstr
+print sum
+
+
 
