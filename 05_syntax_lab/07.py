eae7fe7d4c66953c7d8f62fb9afd2826d24f49c2
SUM2 <alonshkalim@gmail.com>
2016-07-06 22:20:15 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,8 +1,20 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
 
-After each guess print a hint "too large" or "too small" to the user.
 
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+from random import randint
+unknown = randint(1,101)
+print unknown
 
+while True:
+    s = int(raw_input("Please pick a number\n"))
+    cube = randint(1,4) #sometime the user will get "too big"
+    if cube == 1:
+        print "too BIG;)"
+    elif cube == 2:
+        print "too SMALL;)" #sometime the user will get "too small"
+    elif s > unknown:
+        print "too BIG"
+    elif s < unknown:
+        print "too SMALL"
+    else:
+        print "You are RIGHT"
+        break
