31446975a4cd55e3c7ddc917e39ceda8978c5582
SUM2 <alonshkalim@gmail.com>
2016-07-24 12:01:46 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -1,6 +1,6 @@
-"""
-Write a function that takes minlen and
-a list of words, and returns only the words
-longer than minlen
-"""
-
+def longer_than(num,*args):
+	lst = []
+	for i in args:
+		if len(i) > num:
+			lst.append(i)
+	return lst
